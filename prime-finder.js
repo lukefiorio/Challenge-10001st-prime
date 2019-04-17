38a8c36816f247993d7aef8dcf29af26ac4fee40
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-04-17 10:18:03 -1000

    initial solution

--- prime-finder.js
+++ prime-finder.js
@@ -1,3 +1,30 @@
 module.exports = function(n) {
   // do work here
 };
+
+function findPrime(n) {
+  function isPrime(candidate) {
+    // loop up to sqrt of a number to determine whether it's prime
+    for (i = 2; i ** 2 <= candidate; i++) {
+      // if divisible by any numbers along the way, then not prime
+      if (candidate % i === 0 || candidate === 1) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  let primeCounter = 0;
+  let candidates = 2;
+  while (primeCounter < n) {
+    if (isPrime(candidates)) {
+      primeCounter++;
+      candidates++;
+    } else {
+      candidates++;
+    }
+  }
+  console.log(candidates - 1);
+}
+
+findPrime(10000);
